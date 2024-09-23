3eb63da6035f36b93a2475ae53eeac0d86e777b1
Vlad Golubev <holybay1886@gmail.com>
2024-09-23 15:40:05 +0300

    Added and implemented the "Create" method to Recipient Storage.

--- src/main/java/by/it_academy/jd2/golubev_107/mail_sender/storage/IRecipientStorage.java
+++ src/main/java/by/it_academy/jd2/golubev_107/mail_sender/storage/IRecipientStorage.java
@@ -4,5 +4,7 @@ import by.it_academy.jd2.golubev_107.mail_sender.storage.entity.Recipient;
 
 public interface IRecipientStorage {
 
+    Recipient create(Recipient recipient);
+
     Recipient readById(Long id);
 }
--- src/main/java/by/it_academy/jd2/golubev_107/mail_sender/storage/impl/RecipientStorage.java
+++ src/main/java/by/it_academy/jd2/golubev_107/mail_sender/storage/impl/RecipientStorage.java
@@ -1,5 +1,6 @@
 package by.it_academy.jd2.golubev_107.mail_sender.storage.impl;
 
+import by.it_academy.jd2.golubev_107.mail_sender.platform.DBUtil;
 import by.it_academy.jd2.golubev_107.mail_sender.storage.IRecipientStorage;
 import by.it_academy.jd2.golubev_107.mail_sender.storage.connection.IConnectionManager;
 import by.it_academy.jd2.golubev_107.mail_sender.storage.entity.Recipient;
@@ -11,6 +12,8 @@ import java.sql.SQLException;
 
 public class RecipientStorage implements IRecipientStorage {
 
+    private static final String INSERT_RECIPIENT_QUERY = "INSERT INTO app.recipient (type_id, email) VALUES(?, ?) RETURNING id;";
+    private static final String SELECT_REC_TYPE_ID_QUERY = "SELECT rt.id FROM app.recipient_type rt WHERE rt.type = ?";
     private static final String SELECT_READ_BY_ID = """
             SELECT r.id, r.email, rt.type
             FROM app.recipient r
@@ -20,6 +23,54 @@ public class RecipientStorage implements IRecipientStorage {
 
     public RecipientStorage(IConnectionManager connectionManager) {
         this.connectionManager = connectionManager;
+    }
+
+    @Override
+    public Recipient create(Recipient recipient) {
+        Connection connection = null;
+        try {
+            connection = connectionManager.getConnection();
+            connection.setAutoCommit(false);
+            DBUtil.transactionBegin(connection);
+
+            Long recTypeId = null;
+            try (PreparedStatement selRecTypeIdStmt = connection.prepareStatement(SELECT_REC_TYPE_ID_QUERY)) {
+                selRecTypeIdStmt.setString(1, recipient.getType().name());
+                try (ResultSet rs = selRecTypeIdStmt.executeQuery();) {
+                    if (rs.next()) {
+                        recTypeId = rs.getLong("id");
+                    }
+                }
+                if (recTypeId == null) {
+                    DBUtil.transactionRollback(connection);
+                    throw new IllegalStateException("Didn't receive the ID for the provided recipient type!");
+                }
+                selRecTypeIdStmt.clearParameters();
+            }
+
+            Long recipientId = null;
+            try (PreparedStatement insrtRecptStmt = connection.prepareStatement(INSERT_RECIPIENT_QUERY)) {
+                insrtRecptStmt.setLong(1, recTypeId);
+                insrtRecptStmt.setString(2, recipient.getEmailAddress());
+                try (ResultSet rs = insrtRecptStmt.executeQuery();) {
+                    if (rs.next()) {
+                        recipientId = rs.getLong("id");
+                    }
+                }
+                if (recipientId == null) {
+                    DBUtil.transactionRollback(connection);
+                    throw new IllegalStateException("Didn't receive the ID for the saved recipient!" + recipient);
+                }
+                insrtRecptStmt.clearParameters();
+            }
+            connection.commit();
+            return readById(recipientId);
+        } catch (SQLException e) {
+            DBUtil.transactionRollback(connection);
+            throw new RuntimeException("Failed to create a recipient!" + e);
+        } finally {
+            DBUtil.connectionClose(connection);
+        }
     }
 
     @Override
