915dbdb9d9547991e267e2fde30c952d696c7e16
Vlad Golubev <holybay1886@gmail.com>
2024-09-25 18:55:24 +0300

    Designed RecipientAddress Service Interface and added an impl. Created a RecipientAddress DTO class.

---
+++ src/main/java/by/it_academy/jd2/golubev_107/mail_sender/service/IRecipientAddressService.java
@@ -0,0 +1,10 @@
+package by.it_academy.jd2.golubev_107.mail_sender.service;
+
+import by.it_academy.jd2.golubev_107.mail_sender.service.dto.RecipientAddressDto;
+import by.it_academy.jd2.golubev_107.mail_sender.storage.entity.RecipientAddress;
+
+public interface IRecipientAddressService {
+
+    RecipientAddress create(RecipientAddressDto dto);
+
+}
